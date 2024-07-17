3293896ceab80c2861bd91ce093d85a8d41dfc61
ronak1603 <ronakmalani2002@gmail.com>
2024-07-17 23:57:02 +0530

    login oage Ui & functionality done

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,10 +4,19 @@ import { useMutation } from "@tanstack/react-query";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
 import { auth, firestore } from "@/services/firebase";
-import { TextField, Button, Container } from "@mui/material";
+import {
+  TextField,
+  Button,
+  Container,
+  Box,
+  Typography,
+  CircularProgress,
+} from "@mui/material";
 import Link from "next/link";
 import { useForm } from "@mantine/form";
 import { validateEmail } from "@/utils/validations";
+import Image from "next/image";
+import authImage from "@/assets/images/authImage.svg";
 
 interface LoginData {
   email: string;
@@ -34,7 +43,7 @@ const login = async (email: string, password: string): Promise<string> => {
 const LoginPage = () => {
   const router = useRouter();
 
-  const { mutate, isPending } = useMutation({
+  const { mutate, isLoading } = useMutation({
     mutationFn: (data: LoginData) => {
       return login(data.email, data.password);
     },
@@ -66,62 +75,127 @@ const LoginPage = () => {
   };
 
   return (
-    <Container maxWidth="sm">
-      <form
-        onSubmit={handleSubmit}
-        className="space-y-4 w-full max-w-2xl shadow-lg p-10"
+    <Container
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        height: "100vh",
+        width: "100vw",
+      }}
+      style={{ padding: "0px" }}
+      maxWidth="xl"
+    >
+      <Box
+        className="flex w-full justify-center items-center bg-blue-400"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "16px",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "#60a5fa",
+          padding: "0px",
+        }}
       >
-        <h1 className="font-secondary text-xl text-center font-semibold text-[#0b3a65ff]">
-          Chat<span className="font-bold text-[#eeab63ff]">Hub</span>
-        </h1>
-
-        <div>
-          <TextField
-            {...loginForm.getInputProps("email")}
-            fullWidth
-            margin="normal"
-            variant="outlined"
-            label="Email"
-          />
-          {loginForm.errors.email && (
-            <span className="text-red-500">{loginForm.errors.email}</span>
-          )}
-        </div>
-
-        <div>
-          <TextField
-            {...loginForm.getInputProps("password")}
-            fullWidth
-            margin="normal"
-            variant="outlined"
-            label="Password"
-            type="password"
-          />
-          {loginForm.errors.password && (
-            <span className="text-red-500">{loginForm.errors.password}</span>
-          )}
-        </div>
-
-        <div>
-          <Button
-            type="submit"
-            onClick={() => handleSubmit}
-            className="bg-[#0b3a65ff] text-white"
+        <Typography
+          variant="h4"
+          color="white"
+          sx={{ fontSize: "32px", fontWeight: 600, textAlign: "center" }}
+        >
+          Connect Instantly, Chat Seamlessly,
+        </Typography>
+        <Typography
+          variant="h4"
+          color="white"
+          sx={{ fontSize: "32px", fontWeight: 600, textAlign: "center" }}
+        >
+          Anytime, Anywhere.
+        </Typography>
+        <Image src={authImage} alt="authImage" />
+      </Box>
+      <Box className="flex w-full justify-center items-center">
+        <form onSubmit={handleSubmit} className="space-y-4 shadow-lg p-10">
+          <Typography
+            variant="h4"
+            color="white"
+            sx={{
+              fontSize: "32px",
+              fontWeight: 600,
+              textAlign: "center",
+              color: "#60a5fa",
+            }}
           >
-            Login
-          </Button>
-        </div>
+            Chat<span className="font-bold text-[#eeab63ff]">Hub</span>
+          </Typography>
 
-        <span>
-          Not have an account?{" "}
-          <Link
-            href="/register"
-            className="text-blue-600 hover:text-blue-800 hover:underline"
+          <Box
+            sx={{ display: "flex", flexDirection: "column", height: "80px" }}
           >
-            sign up
-          </Link>
-        </span>
-      </form>
+            <TextField
+              {...loginForm.getInputProps("email")}
+              fullWidth
+              margin="normal"
+              variant="outlined"
+              label="Email"
+              className="w-96 h-12"
+            />
+            <Box>
+              {loginForm.errors.email && (
+                <Typography className="text-red-500">
+                  {loginForm.errors.email}
+                </Typography>
+              )}
+            </Box>
+          </Box>
+          <Box
+            sx={{ display: "flex", flexDirection: "column", height: "80px" }}
+          >
+            <TextField
+              {...loginForm.getInputProps("password")}
+              fullWidth
+              margin="normal"
+              variant="outlined"
+              label="Password"
+              type="password"
+              className="w-96 h-12"
+            />
+            <Box>
+              {loginForm.errors.password && (
+                <Typography className="text-red-500">
+                  {loginForm.errors.password}
+                </Typography>
+              )}
+            </Box>
+          </Box>
+          <Box style={{ marginTop: "24px" }}>
+            <Button
+              type="submit"
+              sx={{
+                background: isLoading ? "#9ca3af" : "#60a5fa",
+                color: "white",
+                width: "384px",
+                textTransform: "none",
+                height: "48px",
+                cursor: isLoading ? "not-allowed" : "pointer",
+                "&:hover": {
+                  background: "#60a5fa",
+                },
+              }}
+              onClick={() => handleSubmit()}
+              endIcon={isLoading ? <CircularProgress size={20} /> : null}
+              disabled={isLoading}
+            >
+              {isLoading ? "Login..." : "Login"}
+            </Button>
+          </Box>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            Don&apos;t have an account?&nbsp;
+            <Link href="/register" className="text-[#60a5fa] underline">
+              Register
+            </Link>
+          </Box>
+        </form>
+      </Box>
     </Container>
   );
 };
