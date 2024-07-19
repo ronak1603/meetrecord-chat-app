8cd8bb9d18da3c833f985e3cbdb294860f75dc4f
ronak1603 <ronakmalani2002@gmail.com>
2024-07-19 13:12:03 +0530

    Ui update and functionality update in chatroom

--- src/app/chats/[linkId]/page.tsx
+++ src/app/chats/[linkId]/page.tsx
@@ -1,31 +1,48 @@
 "use client";
 import { useAuth } from "@/context/AuthContext";
 import useChatMessages from "@/services/useChatMessages";
-import { TextField, Button, Box, Container, Typography } from "@mui/material";
+import {
+  TextField,
+  Button,
+  Box,
+  Container,
+  Typography,
+  Avatar,
+  CircularProgress,
+} from "@mui/material";
 import { useState, useEffect } from "react";
 import { useParams, useRouter } from "next/navigation";
 
-const ChatRoom: React.FC = () => {
-  const { user, linkId } = useAuth();
+const ChatRoom = () => {
+  const { user, linkId, userDetails } = useAuth();
   const params = useParams();
   const router = useRouter();
   const { linkId: roomId } = params;
-  const { dbmessages, sendMessage, isLoading, isSending } = useChatMessages(
-    (roomId as string) || (linkId as string)
-  );
+  const { dbmessages, sendMessage, isLoading, isSending, checkRoomIdExists } =
+    useChatMessages((roomId as string) || (linkId as string));
   const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const verifyRoomId = async () => {
+      const doesExist = await checkRoomIdExists(roomId as string);
+      if ((!roomId && !linkId) || !doesExist) {
+        router.push("/dashboard");
+      }
+    };
+
+    verifyRoomId();
+  }, [roomId, linkId, router]);
 
   const handleSendMessage = () => {
     if (message.trim() === "") return;
-    sendMessage({ senderId: user?.uid || "", text: message });
+    sendMessage({
+      senderId: user?.uid || "",
+      text: message,
+      avatarUrl: userDetails?.avatarUrl || "",
+      name: userDetails?.name || "",
+    });
     setMessage("");
   };
-
-  useEffect(() => {
-    if (!roomId && !linkId) {
-      router.push("/dashboard");
-    }
-  }, [roomId, linkId, router]);
 
   useEffect(() => {
     if (!isLoading) {
@@ -34,45 +51,139 @@ const ChatRoom: React.FC = () => {
         chatContainer.scrollTop = chatContainer.scrollHeight;
       }
     }
-  }, [dbmessages, isLoading]);
+  }, [dbmessages]);
 
   return (
-    <Container className="flex flex-col h-full">
-      <Box id="chat-container" className="flex-1 overflow-y-auto p-4 border-b">
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        background:
+          "linear-gradient(45deg, #1E3A8A, #1E3A8A 50%, #ffffff 50px);",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vw",
+        justifyContent: "center",
+      }}
+      style={{ padding: "0px" }}
+      maxWidth="xl"
+    >
+      <Box
+        id="chat-container"
+        sx={{
+          flex: 1,
+          overflowY: "auto",
+          p: 2,
+          my: 4,
+          width: "100vw",
+          borderBottom: 1,
+          borderColor: "divider",
+          boxShadow: "0 25px 50px -12px rgb(0 0 0 / 0.25);",
+          maxWidth: "50%",
+          minHeight: "100vh",
+          background: "#fff",
+          paddingBottom: "100px",
+        }}
+      >
         {dbmessages?.map((msg) => (
           <Box
             key={msg.id}
-            className={`mb-2 ${msg.senderId === user?.uid ? "text-right" : ""}`}
+            sx={{
+              display: "flex",
+              alignItems: "flex-start",
+              mb: 2,
+              gap: 2,
+              flexDirection: msg.senderId === user?.uid ? "row-reverse" : "row",
+            }}
           >
+            {msg.senderId !== user?.uid && (
+              <Avatar
+                src={msg.avatarUrl}
+                alt="user-avatar"
+                sx={{ width: 40, height: 40 }}
+              />
+            )}
             <Box
-              className={`inline-block px-4 py-2 rounded-lg ${
-                msg.senderId === user?.uid
-                  ? "bg-blue-500 text-white"
-                  : "bg-gray-300"
-              }`}
+              sx={{
+                maxWidth: "75%",
+                bgcolor: msg.senderId === user?.uid ? "#1E3A8A" : "grey.300",
+                color:
+                  msg.senderId === user?.uid
+                    ? "primary.contrastText"
+                    : "text.primary",
+                borderRadius: 1,
+                p: 1,
+              }}
             >
-              <Typography>{msg.text}</Typography>
+              {msg.senderId !== user?.uid && (
+                <Typography sx={{ fontWeight: "600" }}>{msg.name}</Typography>
+              )}
+
+              <Typography sx={{ flexWrap: "wrap", wordBreak: "break-word" }}>
+                {msg.text}
+              </Typography>
             </Box>
           </Box>
         ))}
       </Box>
-      <Box className="p-4 border-t">
+      <Box
+        sx={{
+          display: "flex",
+          gap: "16px",
+          p: 2,
+          position: "fixed",
+          bottom: 0,
+          zIndex: 50,
+          borderTop: 1,
+          width: "100%",
+          borderColor: "divider",
+          background: "#fff",
+          maxWidth: "50%",
+        }}
+      >
         <TextField
-          fullWidth
-          variant="outlined"
           value={message}
           onChange={(e) => setMessage(e.target.value)}
           placeholder="Type your message..."
-          className="mb-2"
+          sx={{
+            display: "flex",
+            flexGrow: 1,
+            "& .MuiOutlinedInput-root": {
+              borderRadius: 2,
+              "& fieldset": {
+                borderColor: "#1E3A8A",
+              },
+              "&:hover fieldset": {
+                borderColor: "#1E3A8A",
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: "none",
+              },
+            },
+          }}
         />
         <Button
-          fullWidth
           variant="contained"
-          color="primary"
+          color="secondary"
+          sx={{
+            textTransform: "none",
+            height: "60px",
+            fontSize: "24px",
+            fontWeight: "500",
+            borderRadius: "50%",
+            color: "#fff",
+            boxShadow: "0",
+            background: isSending ? "#9ca3af" : "#1E3A8A",
+
+            "&:hover": {
+              background: "#1E3A8A",
+            },
+          }}
           onClick={handleSendMessage}
+          endIcon={isSending ? <CircularProgress size={20} /> : null}
           disabled={isSending}
         >
-          Send
+          ↑
         </Button>
       </Box>
     </Container>
--- src/services/useChatMessages.ts
+++ src/services/useChatMessages.ts
@@ -5,16 +5,23 @@ import {
   getDocs,
   query,
   orderBy,
-  QueryDocumentSnapshot,
+  doc,
+  writeBatch,
   DocumentData,
+  QueryDocumentSnapshot,
+  setDoc,
+  getDoc,
 } from "firebase/firestore";
 import { firestore } from "@/services/firebase";
+import { useRouter } from "next/navigation";
 
 interface Message {
   id: string;
   senderId: string;
   text: string;
+  avatarUrl: string;
   timestamp: Date;
+  name: string;
 }
 
 const fetchMessages = async (linkId: string): Promise<Message[]> => {
@@ -27,37 +34,76 @@ const fetchMessages = async (linkId: string): Promise<Message[]> => {
     id: doc.id,
     senderId: doc.data().senderId,
     text: doc.data().text,
+    avatarUrl: doc.data().avatarUrl,
+    name: doc.data().name,
     timestamp: doc.data().timestamp.toDate(),
   }));
 };
 
-const useChatMessages = (linkId: string) => {
+const deleteChatRoom = async (linkId: string) => {
+  try {
+    const batch = writeBatch(firestore);
+    const messagesCollection = collection(
+      firestore,
+      "chatRooms",
+      linkId,
+      "messages"
+    );
+    const messagesQuerySnapshot = await getDocs(messagesCollection);
+    messagesQuerySnapshot.forEach((messageDoc) => {
+      batch.delete(messageDoc.ref);
+    });
+    const chatRoomDoc = doc(firestore, "chatRooms", linkId);
+    batch.delete(chatRoomDoc);
+    await batch.commit();
+  } catch (error) {
+    console.error("Error deleting chat room:", error);
+    throw new Error("Failed to delete chat room");
+  }
+};
+
+const createChatRoom = async (linkId: string) => {
+  const newRoomRef = doc(firestore, "chatRooms", linkId);
+  await setDoc(newRoomRef, {
+    createdAt: new Date(),
+  });
+  return newRoomRef.id;
+};
+
+const useChatMessages = (linkId: string | null) => {
+  const router = useRouter();
   const queryClient = useQueryClient();
+
   const messagesQuery = useQuery<Message[], Error>(
     ["messages", linkId],
-    () => fetchMessages(linkId),
+    () => fetchMessages(linkId as string),
     {
       enabled: !!linkId,
     }
   );
 
-  const { mutate: sendMessage, isLoading: isSending } = useMutation<
-    void,
-    Error,
-    { senderId: string; text: string }
-  >({
+  const { mutate: sendMessage, isLoading: isSending } = useMutation({
     mutationFn: async ({
       senderId,
       text,
+      avatarUrl,
+      name,
     }: {
       senderId: string;
       text: string;
+      avatarUrl: string;
+      name: string;
     }) => {
-      await addDoc(collection(firestore, "chatRooms", linkId, "messages"), {
-        senderId,
-        text,
-        timestamp: new Date(),
-      });
+      await addDoc(
+        collection(firestore, "chatRooms", linkId as string, "messages"),
+        {
+          senderId,
+          text,
+          avatarUrl,
+          name,
+          timestamp: new Date(),
+        }
+      );
     },
     onSuccess: () => {
       queryClient.invalidateQueries(["messages", linkId]);
@@ -67,11 +113,45 @@ const useChatMessages = (linkId: string) => {
     },
   });
 
+  const { mutate: deleteRoom } = useMutation<void, Error, string>({
+    mutationFn: deleteChatRoom,
+    onSuccess: () => {
+      router.push("/dashboard");
+      queryClient.invalidateQueries(["messages", linkId]);
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+
+  const { mutate: createRoom } = useMutation<string, Error, string>({
+    mutationFn: (linkid: string) => createChatRoom(linkid),
+    onSuccess: (data) => {
+      // queryClient.invalidateQueries(["messages", data]);
+      router.push(`/chats/${data}`);
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+
+  const checkRoomIdExists = async (roomId: string): Promise<boolean> => {
+    try {
+      const roomDoc = await getDoc(doc(firestore, "chatRooms", roomId));
+      return roomDoc.exists();
+    } catch (error) {
+      return false;
+    }
+  };
+
   return {
     dbmessages: messagesQuery.data,
     sendMessage,
     isLoading: messagesQuery.isLoading,
     isSending,
+    deleteRoom,
+    createRoom,
+    checkRoomIdExists,
   };
 };
 
